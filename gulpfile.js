1cf9246c7beed573c8af9e38ba89012276075f8d
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-31 15:12:08 -0700

    Adds uglify to the concatenation. Uses minified js file in index. #1.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
   return gulp.src(['./app/app.module.js', './app/app.config.js', './app/**/*.js'])
-    .pipe(concat('all.js'))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./public/'))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./public/'));
 });
