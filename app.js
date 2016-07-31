651554899d143757a4ca1f201b26f0766109bb6d
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-31 08:33:22 -0700

    Sets up simple express server and index.

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+
+app.set("port", (process.env.PORT || 8000));
+
+app.use(express.static("./public"));
+
+app.listen(app.get("port"), () => console.log(`Listening on port:${app.get("port")}`));
