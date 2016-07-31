6a7bdffe7d0d2744b28302a57ce47d81a063f134
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-31 15:14:23 -0700

    Adds images for examples and more examples to list. #1

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -6,7 +6,12 @@ angular
 
   function HomeController() {
     var vm = this;
-    vm.list = [{name: "name"},{name: "test"}];
+    vm.list = [
+      {name: "binary", url: "img/binary-sm.jpg", alt:"binary"},
+      {name: "test", url: "img/head-sm.jpg", alt:"head"},
+      {name: "fractal", url: "img/fractal-sm.jpg", alt: "fractal"},
+      {name: "geometry", url: "img/geometry-sm.jpg", alt: "geometry"}
+     ];
     vm.message = "Examples";
     activate();
 
